84705a01e6fcb8519462e4fe5eeae6f4d6dafc66
Yannic Brügger <yannic@bruegger.dev>
2022-03-09 21:25:55 +0100

    feat: :sparkles: initialize new project with cross-fade-viewer component

---
+++ src/ImagePreview.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="image-preview" @click="$emit('click')" @keydown="$emit('click')">
+    <div class="image">
+      <img alt="preview of choosable image" :src="src">
+    </div>
+    <dl class="info">
+      <dt>{{title}}</dt>
+    </dl>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    src: { type: String },
+    title: { type: String },
+  },
+  emits: [
+    'click',
+  ],
+});
+</script>
