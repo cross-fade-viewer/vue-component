338bcccde48215057c208badaef307d0bdadec1c
Valeria Orlova <v.orlova@sprinteins.com>
2022-03-16 14:20:46 +0100

    build: fix 3

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,14 +1,5 @@
-/*
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/HelloWorld.vue';
-
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe('Test Base', () => {
+  it('returns a message', () => {
+    console.log('Hello! Write tests here');
   });
 });
-*/
